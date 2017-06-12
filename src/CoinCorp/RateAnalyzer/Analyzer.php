f72c60d9369feddd159ec2ce72ee1e5bd1b6cc17
Ivan Pushkin <iv.pushk@gmail.com>
2017-06-12 16:06:32 +0300

    Create MACD test

--- src/CoinCorp/RateAnalyzer/Analyzer.php
+++ src/CoinCorp/RateAnalyzer/Analyzer.php
@@ -17,6 +17,11 @@ class Analyzer
     private $aggregator;
 
     /**
+     * @var \CoinCorp\RateAnalyzer\Candle[][]
+     */
+    private $cache;
+
+    /**
      * Analyzer constructor.
      *
      * @param \CoinCorp\RateAnalyzer\AggregatorInterface $aggregator
@@ -28,7 +33,27 @@ class Analyzer
 
     public function analyze() {
         foreach ($this->aggregator->rows() as $row) {
+            // Update cache
+            foreach ($row as $column => $candle) {
+                if (!is_array($this->cache[$column])) {
+                    $this->cache[$column] = [];
+                }
+                while (sizeof($this->cache[$column]) >= self::CACHE_SIZE) {
+                    array_shift($this->cache[$column]);
+                }
+                array_push($this->cache[$column], $candle);
+            }
 
+            foreach ($this->cache as $column => $candles) {
+                if (sizeof($candles) === self::CACHE_SIZE) {
+                    $real = [];
+                    foreach ($candles as $candle) {
+                        array_push($real, $candle->close);
+                    }
+                    print_r(trader_macd($real, 10, 21, 9));
+                    exit();
+                }
+            }
         }
     }
 }
