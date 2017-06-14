00760680edfced6f273dfa7e122fa09d0aa95049
Ivan Pushkin <pushkin13@bk.ru>
2017-06-14 23:30:52 +0300

    Create TODO

--- src/CoinCorp/RateAnalyzer/ExchangeStateSlice.php
+++ src/CoinCorp/RateAnalyzer/ExchangeStateSlice.php
@@ -13,11 +13,12 @@ class ExchangeStateSlice
      * @var string
      */
     public $title;
-
     /**
      * @var integer[]
      */
     public $verticalLines = [];
+
+    // TODO: Добавить возможность совмещать графики.
 
     /**
      * @var \CoinCorp\RateAnalyzer\Candle[][]
