cb37aae2078b8bad7c968bdbc623835487557612
Ivan Pushkin <pushkin13@bk.ru>
2017-07-23 03:59:43 +0300

    Create trading pairs statistics generators

--- bin/trade-pair-of-pairs.php
+++ bin/trade-pair-of-pairs.php
@@ -12,11 +12,30 @@ use Monolog\Logger;
 
 ini_set("trader.real_precision", 10);
 
+//
+// Constants
+//
 
+define("DEFAULT_PERIOD_SMA_RATIO", 60*24*7);
+
+define("CANDLE_TRADE_PAIR_FIRST", 0);
+define("CANDLE_TRADE_PAIR_SECOND", 1);
+
+define("CACHE_SIZE_DEVIATION", 18);
+
+// Графики на временном срезе
+define("CHART_CLOSE_PRICE_FIRST_PAIR", 0);
+define("CHART_CLOSE_PRICE_SECOND_PAIR", 1);
+define("CHART_DEVIATION", 2);
+define("CHART_REAL_PAIRS_DIFF", 3);
+define("CHART_REAL_RATIO_DIVIDE_RATIO_SMA", 4);
+
+//
+// Commands
+//
 
 $cmd = new Command();
-
-$cmd->option('s')->aka('sources')->describedAs('Config file with list of sources')->required()->file();
+$cmd->option('s')->aka('sources')->describedAs('Config file with 2 sources')->required()->file();
 $cmd->option('b')->aka('batch-size')->describedAs('Candles number to batch')->default(1)->must(function($value) {
     return is_numeric($value) && $value > 0;
 });
@@ -26,34 +45,53 @@ $cmd->option('from')->describedAs('Time UTC')->default(new DateTime("0001-01-01"
 $cmd->option('to')->describedAs('Time UTC')->default(new DateTime('now', new DateTimeZone('UTC')))->cast(function($value) {
     return new DateTime($value, new DateTimeZone('UTC'));
 });
+$cmd->option('period-sma-ratio')->describedAs('Period for SMA(pairs ratio) in minutes')->default(DEFAULT_PERIOD_SMA_RATIO)->must(function($value) {
+    return is_integer($value) && $value > 1 && $value <= 100000;
+});
+// TODO: Использовать эти значения.
+$cmd->option('first-fee')->describedAs('First pair trade fee')->default(0)->must(function($value) {
+    return is_double($value);
+});
+$cmd->option('second-fee')->describedAs('Second pair trade fee')->default(0)->must(function($value) {
+    return is_double($value);
+});
 
-
+/** @var \CoinCorp\RateAnalyzer\CandleEmitterInterface[] $sources */
+$sources = require $cmd['sources'];
+/** @var integer $batchSize */
+$batchSize = intval($cmd['batch-size']);
 /** @var \DateTime $from */
 $from = $cmd['from'];
 /** @var \DateTime $to */
 $to = $cmd['to'];
+// За какой период считать средние соотношение соотношение курсов
+/** @var integer $periodRatioSMA */
+$periodRatioSMA = intval($cmd['period-sma-ratio']/$batchSize);
+$cacheSizeRatio = $periodRatioSMA;
+$cacheSizeRatioTail = $periodRatioSMA - 1;
+// TODO: Использовать эти значения.
+/** @var double $firstPairFee */
+$firstPairFee = doubleval($cmd['first-fee']);
+/** @var double $secondPairFee */
+$secondPairFee = doubleval($cmd['second-fee']);
 
-
-
+//
+// Init logging
+//
 
 $logger = new Logger('logger');
+$logger->info("Start calculation");
+
+//
+// Init aggregator
+//
+
 $aggregator = new CandleAggregator($logger);
-/** @var \CoinCorp\RateAnalyzer\CandleEmitterInterface[] $sources */
-$sources = require $cmd['sources'];
-
-if (sizeof($sources) < 2) {
-    $logger->error("Invalid source count < 2", ['count' => sizeof($sources)]);
-    exit(1);
-}
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                ДОБАВЛЕНИЕ ИСТОЧНИКОВ ДЛЯ АГГРЕГАЦИИ                                 *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 foreach ($sources as $source) {
     if ($source instanceof CandleSource) {
-        if ($cmd['batch-size'] > 1) {
-            $aggregator->addCandleEmitter(new CandleBatcher($source, (integer)$cmd['batch-size']));
+        if ($batchSize > 1) {
+            $aggregator->addCandleEmitter(new CandleBatcher($source, $batchSize));
         } else {
             $aggregator->addCandleEmitter($source);
         }
@@ -68,303 +106,163 @@ if ($aggregator->capacity() !== 2) {
 $generator = $aggregator->rows();
 if (!$generator->valid()) {
     $logger->warn("Empty aggregator");
-    exit;
+    exit(1);
 }
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                          ПОДГОТОВКА                                                 *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/** @var \CoinCorp\RateAnalyzer\DataRow $dataRow */
+$dataRow = $generator->current();
+$firstPair = $dataRow->candles[CANDLE_TRADE_PAIR_FIRST];
+$secondPair = $dataRow->candles[CANDLE_TRADE_PAIR_SECOND];
+$logger->info("Pair labels", [
+    'first_pair_label'  => $firstPair->label,
+    'second_pair_label' => $secondPair->label,
+]);
 
-define("PAIR_FIRST", 0);
-define("PAIR_SECOND", 1);
+//
+// Cache ratio values
+//
 
-define("PERIOD_SMA_PRICE_SHORT", 4);
-define("PERIOD_SMA_PRICE_MEDIUM", 9);
-define("PERIOD_SMA_PRICE_LONG", 18);
+$logger->info("Cache ratio values");
 
-define("PERIOD_SMA_RATIO", 20);
-
-define("PRICE_TAIL_CACHE_SIZE", max(PERIOD_SMA_PRICE_SHORT, PERIOD_SMA_PRICE_MEDIUM, PERIOD_SMA_PRICE_LONG, PERIOD_SMA_RATIO)-1);
-define("PRICE_CACHE_SIZE", max(PERIOD_SMA_PRICE_SHORT, PERIOD_SMA_PRICE_MEDIUM, PERIOD_SMA_PRICE_LONG, PERIOD_SMA_RATIO));
-
-
-define("PERIOD_SMA_DEVIATION_PLUS", 18);
-define("PERIOD_SMA_DEVIATION_MINUS", 18);
-define("PERIOD_ADX_DEVIATION", 3);
-define("DEVIATION_CACHE_SIZE", max(PERIOD_SMA_DEVIATION_PLUS, PERIOD_SMA_DEVIATION_MINUS, PERIOD_ADX_DEVIATION*2));
-
-
-
-$tailFirstPriceClose = [];
-$tailSecondPriceClose = [];
+/** @var float[] $tailRatioPrices */
 $tailRatioPrices = [];
 
-for ($i = 0; $i < PRICE_TAIL_CACHE_SIZE; $i++) {
+/** @var float[] $ratioPrices */
+$ratioPrices = [];
+
+for ($i = 0; $i < $cacheSizeRatioTail+$cacheSizeRatio; $i++) {
+    // *** Begin cache ratio ***
     /** @var \CoinCorp\RateAnalyzer\DataRow $dataRow */
     $dataRow = $generator->current();
+    $firstPair = $dataRow->candles[CANDLE_TRADE_PAIR_FIRST];
+    $secondPair = $dataRow->candles[CANDLE_TRADE_PAIR_SECOND];
+    $ratio = $firstPair->close / $secondPair->close;
+    array_push($ratioPrices, $ratio);
+    while (sizeof($ratioPrices) > $cacheSizeRatio) {
+        array_push($tailRatioPrices, array_shift($ratioPrices));
+        if (sizeof($tailRatioPrices) > $cacheSizeRatioTail) {
+            array_shift($tailRatioPrices);
+        }
+    }
+    // *** End cache ratio ***
 
-    $firstPair = $dataRow->candles[PAIR_FIRST];
-    array_push($tailFirstPriceClose, $firstPair->close);
-
-    $secondPair = $dataRow->candles[PAIR_SECOND];
-    array_push($tailSecondPriceClose, $secondPair->close);
-
-    // Ratio prices
-    array_push($tailRatioPrices, $firstPair->close/$secondPair->close);
-
+    // *** Begin finalize iteration ***
     $generator->next();
     if (!$generator->valid()) {
-        break;
+        $logger->warn("Closed generator");
+        exit(1);
     }
+    // *** End finalize iteration ***
 }
 
-$generator = $aggregator->rows();
-if (!$generator->valid()) {
-    $logger->warn("Empty aggregator");
-    exit;
-}
+//
+// Init time slice
+//
 
-
-
-
-
+$slice = new ExchangeStateSlice;
+/** @var \CoinCorp\RateAnalyzer\DataRow $dataRow */
 $dataRow = $generator->current();
-$firstPair = $dataRow->candles[PAIR_FIRST];
-$secondPair = $dataRow->candles[PAIR_SECOND];
-
-
-$slice = new ExchangeStateSlice();
-$slice->seriesNames = ["Deviation", "Deviation DI+", "Deviation DI-",
-    "Common deposit BTC", "Price ".$firstPair->label, "Price ".$secondPair->label];
+$firstPair = $dataRow->candles[CANDLE_TRADE_PAIR_FIRST];
+$secondPair = $dataRow->candles[CANDLE_TRADE_PAIR_SECOND];
+$slice->seriesNames = [
+    CHART_CLOSE_PRICE_FIRST_PAIR      => "Close price ".$firstPair->label,
+    CHART_CLOSE_PRICE_SECOND_PAIR     => "Close price ".$secondPair->label,
+    CHART_DEVIATION                   => "Deviation",
+    CHART_REAL_PAIRS_DIFF             => "Real diff",
+    CHART_REAL_RATIO_DIVIDE_RATIO_SMA => sprintf("Ratio/SMA_%d(Ratio)", $periodRatioSMA),
+];
 $slice->series = array_fill(0, sizeof($slice->seriesNames), []);
 
+//
+// Calculate statistics
+//
 
-
-$firstPriceClose = [];
-$secondPriceClose = [];
-$ratioPrices = [];
+/** @var float[] $deviations */
 $deviations = [];
-$state = 0;
 
-
-
-///////////////////// Emulation
-$firstBTC = 1.0;
-$depositFirst = 0.0;
-$positionFirst = null;
-
-$secondBTC = 1.0;
-$depositSecond = 0.0;
-$positionSecond = null;
-
-$dataRow = $generator->current();
-$firstPair = $dataRow->candles[PAIR_FIRST];
-$secondPair = $dataRow->candles[PAIR_SECOND];
-$startBTC = $firstBTC + $secondBTC + $depositFirst*$firstPair->close + $depositSecond*$secondPair->close;
-//////////////////////////////
-
-
-$green = 0;
-$red = 0;
-
-$fee = 0.002;
-$feeResult = 0.0;
-
-
-
-while (true) {
+while ($generator->valid()) {
+    // *** Begin prepare ratio caches ***
     /** @var \CoinCorp\RateAnalyzer\DataRow $dataRow */
     $dataRow = $generator->current();
-
-    $firstPair = $dataRow->candles[PAIR_FIRST];
-    array_push($firstPriceClose, $firstPair->close);
-    while (sizeof($firstPriceClose) > PRICE_CACHE_SIZE) {
-        array_push($tailFirstPriceClose, array_shift($firstPriceClose));
-        array_shift($tailFirstPriceClose);
-    }
-
-    $secondPair = $dataRow->candles[PAIR_SECOND];
-    array_push($secondPriceClose, $secondPair->close);
-    while (sizeof($secondPriceClose) > PRICE_CACHE_SIZE) {
-        array_push($tailSecondPriceClose, array_shift($secondPriceClose));
-        array_shift($tailSecondPriceClose);
-    }
-
-    // Ratio prices
-    array_push($ratioPrices, $firstPair->close/$secondPair->close);
-    while (sizeof($ratioPrices) > PRICE_CACHE_SIZE) {
+    $firstPair = $dataRow->candles[CANDLE_TRADE_PAIR_FIRST];
+    $secondPair = $dataRow->candles[CANDLE_TRADE_PAIR_SECOND];
+    /** @var float $ratio */
+    $ratio = $firstPair->close / $secondPair->close;
+    array_push($ratioPrices, $ratio);
+    while (sizeof($ratioPrices) > $cacheSizeRatio) {
         array_push($tailRatioPrices, array_shift($ratioPrices));
         array_shift($tailRatioPrices);
     }
+    // *** End prepare ratio caches ***
 
-    // -- Begin calculate deviation --
-
-    $ratioPricesSMA = array_values(trader_sma(array_merge($tailRatioPrices, $ratioPrices), PERIOD_SMA_RATIO));
-
+    // *** Begin calculate sigma, deviation and SMA(ratio prices) ***
+    /** @var float[] $ratioPricesSMA */
+    $ratioPricesSMA = array_values(trader_sma(array_merge($tailRatioPrices, $ratioPrices), $periodRatioSMA));
+    /** @var float $ratioSMA */
+    $ratioSMA = $ratioPricesSMA[sizeof($ratioPricesSMA)-1];
     /** @var float $sum */
-    $sum = 0;
+    $sum = 0.0;
     for ($i = 0; $i < sizeof($ratioPrices); $i++) {
-        $sum += pow($ratioPrices[$i] - $ratioPricesSMA[$i], 2);
+        $sum += pow($ratioPrices[$i]-$ratioPricesSMA[$i], 2);
     }
-    $sigma = sqrt($sum/sizeof($ratioPricesSMA));
-    $deviation = ($ratioPrices[sizeof($ratioPrices)-1]-$ratioPricesSMA[sizeof($ratioPricesSMA)-1]) / $sigma;
+    $sigma = sqrt($sum/sizeof($ratioPrices));
+    $deviation = ($ratio-$ratioSMA) / $sigma;
     array_push($deviations, $deviation);
-    while (sizeof($deviations) > DEVIATION_CACHE_SIZE) {
+    while (sizeof($deviations) > CACHE_SIZE_DEVIATION) {
         array_shift($deviations);
     }
+    $logger->info("Stats", [
+        'ratio'     => $ratio,
+        'ratioSMA'  => $ratioSMA,
+        'sigma'     => $sigma,
+        'deviation' => $deviation,
+    ]);
+    // *** End calculate sigma, deviation and SMA(ratio) ***
 
-    // -- End calculate deviation --
-
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-    if (sizeof($deviations) === DEVIATION_CACHE_SIZE) {
-        if ($dataRow->time->getTimestamp() < $from->getTimestamp()) {
-            goto next;
-        }
-        if ($dataRow->time->getTimestamp() > $to->getTimestamp()) {
-            break;
-        }
-
-
-        $deviationsMinus = array_map(function ($value) {
-            return $value > 0 ? 0 : $value;
-        }, $deviations);
-        $deviationsPlus = array_map(function ($value) {
-            return $value < 0 ? 0 : $value;
-        }, $deviations);
-
-        // -- Begin calculate indicators --
-
-        $deviationPlusDI = array_slice(array_values(trader_plus_di($deviations, $deviations, $deviations, PERIOD_ADX_DEVIATION)), -1)[0];
-        $deviationMinusDI = array_slice(array_values(trader_minus_di($deviations, $deviations, $deviations, PERIOD_ADX_DEVIATION)), -1)[0];
-        $deviationSMA4 = array_slice(array_values(trader_sma($deviations, 4)), -1)[0];
-        $deviationSMA9 = array_slice(array_values(trader_sma($deviations, 9)), -1)[0];
-//        $deviationMinusSMA = array_slice(array_values(trader_sma($deviationsMinus, 3)), -1)[0];
-//        $deviationPlusSMA = array_slice(array_values(trader_sma($deviationsPlus, PERIOD_SMA_DEVIATION_PLUS)), -1)[0];
-        $firstPriceSMAShort = array_slice(array_values(trader_sma($firstPriceClose, PERIOD_SMA_PRICE_SHORT)), -1)[0];
-        $firstPriceSMAMedium = array_slice(array_values(trader_sma($firstPriceClose, PERIOD_SMA_PRICE_MEDIUM)), -1)[0];
-        $firstPriceSMALong = array_slice(array_values(trader_sma($firstPriceClose, PERIOD_SMA_PRICE_LONG)), -1)[0];
-        $secondPriceSMAShort = array_slice(array_values(trader_sma($secondPriceClose, PERIOD_SMA_PRICE_SHORT)), -1)[0];
-        $secondPriceSMAMedium = array_slice(array_values(trader_sma($secondPriceClose, PERIOD_SMA_PRICE_MEDIUM)), -1)[0];
-        $secondPriceSMALong = array_slice(array_values(trader_sma($secondPriceClose, PERIOD_SMA_PRICE_LONG)), -1)[0];
-
-        // -- End calculate indicators --
-
-
-        if ($deviation < $deviationSMA4 && $deviationSMA4 < $deviationSMA9 && $deviationSMA9 < 0 && $state != 1
-            && $deviationMinusDI > 65 && $deviationPlusDI < 35) {
-            $state = 1;
-            array_push($slice->verticalLines, [
-                'color' => 'green',
-                'value' => $dataRow->time->getTimestamp(),
-            ]);
-            $green += 1;
-            if ($positionFirst != 'long') {
-                $positionFirst = 'long';
-                $feeResult += $firstBTC / $firstPair->close * $fee;
-
-                $depositFirst += $firstBTC / $firstPair->close;
-                $firstBTC = 0;
-            }
-            if ($positionSecond != 'short') {
-                $positionSecond = 'short';
-                $feeResult += $depositSecond * $secondPair->close * $fee;
-
-                $secondBTC += $depositSecond * $secondPair->close;
-                $depositSecond = 0;
-            }
-        }
-
-        if ($deviation > $deviationSMA4 && $deviationSMA4 > $deviationSMA9 && $deviationSMA9 > 0 && $state != 2
-            && $deviationPlusDI > 65 && $deviationMinusDI < 35) {
-            $state = 2;
-            array_push($slice->verticalLines, [
-                'color' => 'red',
-                'value' => $dataRow->time->getTimestamp(),
-            ]);
-            $red += 1;
-            if ($positionFirst != 'short') {
-                $positionFirst = 'short';
-
-                $feeResult += $depositFirst * $firstPair->close * $fee;
-
-                $firstBTC += $depositFirst * $firstPair->close;
-                $depositFirst = 0;
-            }
-            if ($positionSecond != 'long') {
-                $positionSecond = 'long';
-
-                $feeResult += $secondBTC / $secondPair->close * $fee;
-
-
-                $depositSecond += $secondBTC / $secondPair->close;
-                $secondBTC = 0;
-            }
-        }
-
-
-
-
-        array_push($slice->series[0], [
-            'start' => $dataRow->time->getTimestamp(),
-            'price' => $deviation,
-        ]);
-        array_push($slice->series[1], [
-            'start' => $dataRow->time->getTimestamp(),
-            'price' => $deviationPlusDI,
-        ]);
-        array_push($slice->series[2], [
-            'start' => $dataRow->time->getTimestamp(),
-            'price' => $deviationMinusDI,
-        ]);
-
-        $deposit = $firstBTC + $depositFirst*$firstPair->close + $secondBTC + $depositSecond*$secondPair->close;
-
-        array_push($slice->series[3], [
-            'start' => $dataRow->time->getTimestamp(),
-            'price' => $deposit,
-        ]);
-        array_push($slice->series[4], [
-            'start' => $dataRow->time->getTimestamp(),
-            'price' => $firstPair->close,
-        ]);
-        array_push($slice->series[5], [
-            'start' => $dataRow->time->getTimestamp(),
-            'price' => $secondPair->close,
-        ]);
+    // *** Begin check time range ***
+    if ($dataRow->time->getTimestamp() < $from->getTimestamp()) {
+        $generator->next();
+        continue;
     }
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-    next:
-
-    $generator->next();
-    if (!$generator->valid()) {
+    if ($dataRow->time->getTimestamp() > $to->getTimestamp()) {
         break;
     }
+    // *** End check time range ***
+
+    // *** Begin create charts ***
+    array_push($slice->series[CHART_CLOSE_PRICE_FIRST_PAIR], [
+        'start' => $dataRow->time->getTimestamp(),
+        'price' => $firstPair->close,
+    ]);
+    array_push($slice->series[CHART_CLOSE_PRICE_SECOND_PAIR], [
+        'start' => $dataRow->time->getTimestamp(),
+        'price' => $secondPair->close,
+    ]);
+    array_push($slice->series[CHART_DEVIATION], [
+        'start' => $dataRow->time->getTimestamp(),
+        'price' => $deviation,
+    ]);
+    array_push($slice->series[CHART_REAL_PAIRS_DIFF], [
+        'start' => $dataRow->time->getTimestamp(),
+        'price' => $firstPair->close-$secondPair->close,
+    ]);
+    array_push($slice->series[CHART_REAL_RATIO_DIVIDE_RATIO_SMA], [
+        'start' => $dataRow->time->getTimestamp(),
+        'price' => $ratio/$ratioSMA,
+    ]);
+    // *** Begin create charts ***
+
+    // *** Begin finalize iteration ***
+    $generator->next();
+    if (!$generator->valid()) {
+        $logger->info("Generator closed");
+        break;
+    }
+    // *** End finalize iteration ***
 }
 
-
-/** @var \CoinCorp\RateAnalyzer\DataRow $dataRow */
-//$dataRow = $generator->current();
-
-$firstPair = $dataRow->candles[PAIR_FIRST];
-$secondPair = $dataRow->candles[PAIR_SECOND];
-
-$result = $firstBTC + $depositFirst*$firstPair->close + $secondBTC + $depositSecond*$secondPair->close;
-
-$logger->info("END", [
-    'result' => $result,
-    'start'  => $startBTC,
-    'proc'   => sprintf("%d%%", round($result/$startBTC*100)),
-    'green'  => $green,
-    'red'    => $red,
-    'fee'    => $feeResult,
-]);
+//
+// Print time slice
+//
 
 file_put_contents("php://stdout", json_encode($slice));
